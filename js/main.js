de8fc82868ff6731c097264dfe6a38c4b08ba27e
Luigi <luigismilzo@live.com>
2020-03-30 18:18:34 +0200

    Roll button working

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 // References
 var userIds = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
-// var inputEmail = prompt('Inserisci il tuo indirizzo email');
+var inputEmail = prompt('Inserisci il tuo indirizzo email');
 var userFound = false;
 
 // User matching
@@ -17,32 +17,36 @@ for (var i = 0; i < userIds.length; i++) {
 
 // Feedback
 if (userFound == true) {
-    // alert('Accesso effettuato');
+    alert('Accesso effettuato');
 } else {
-    // alert('Errore di autenticazione: utente non trovato');
+    alert('Errore di autenticazione: utente non trovato');
 }
 
 /*
  * Ex-2
 */
 
+// References
+var rollBtn = document.getElementById('rollBtn');
 
 
-// Player roll
-var playerRoll = Math.floor((Math.random() * 6) + 1);
-console.log(playerRoll);
+rollBtn.addEventListener ( 'click', 
+    function () {
+        // Player roll
+        var playerRoll = Math.floor((Math.random() * 6) + 1);
+        console.log(playerRoll);
 
-// CPU roll
-var cpuRoll = Math.floor((Math.random() * 6) + 1);
-console.log(cpuRoll);
+        // CPU roll
+        var cpuRoll = Math.floor((Math.random() * 6) + 1);
+        console.log(cpuRoll);
 
-// Scores matching
-if ( playerRoll < cpuRoll) {
-    console.log('Hai perso');
-} else if ( playerRoll > cpuRoll ) {
-    console.log('Hai vinto');
-} else {
-    console.log('Parità');
-}
-
-
+        // Scores matching
+        if ( playerRoll < cpuRoll) {
+            console.log('Hai perso');
+        } else if ( playerRoll > cpuRoll ) {
+            console.log('Hai vinto');
+        } else {
+            console.log('Parità');
+        } 
+    }
+);
