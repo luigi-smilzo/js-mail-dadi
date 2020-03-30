afbab165ac811c7a0afb26a2cfd65a79afa5ff9f
Luigi <luigismilzo@live.com>
2020-03-30 17:35:14 +0200

    Dice game raw setup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+/*
+ * Ex-1
+*/
+/*
 // References
 var userIds = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
 var inputEmail = prompt('Inserisci il tuo indirizzo email');
@@ -17,3 +21,25 @@ if (userFound == true) {
 } else {
     console.log('Fail');
 }
+*/
+/*
+ * Ex-2
+*/
+
+// Player roll
+var playerRoll = Math.floor((Math.random() * 6) + 1);
+console.log(playerRoll);
+
+// CPU roll
+var cpuRoll = Math.floor((Math.random() * 6) + 1);
+console.log(cpuRoll);
+
+if ( playerRoll <= cpuRoll) {
+    console.log('Hai perso');
+} else if ( playerRoll >= cpuRoll ) {
+    console.log('Hai vinto');
+} else {
+    console.log('Parità');
+}
+
+
